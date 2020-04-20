99bb101aa3d3e0cd875b1aca9b40f4011046614e
Josh Heyer <shog@stackoverflow.com>
2020-04-20 04:07:14 -0600

    last try

--- postgres-django-setup/step3Data.sh
+++
@@ -1,2 +0,0 @@
-cp -R ~/myproject/myproject/* ~/tutorial/myproject/myproject
-docker cp ~/myproject/myproject/settings.py  `docker ps --filter "ancestor=postgres" -q`:/var/projects/myproject/myproject/settings.py
