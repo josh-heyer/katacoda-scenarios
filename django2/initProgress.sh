512a9c9cde12019e834eb5a6d31db3ebd6e7d3dd
Josh Heyer <shog@stackoverflow.com>
2020-04-24 11:17:10 -0600

    needs to run foreground

--- django2/initProgress.sh
+++ django2/initProgress.sh
@@ -2,5 +2,5 @@
 # katacoda does not handle psql's pager well AT ALL - locks up the terminal about half the time. Get rid of it
 export PAGER=
 # current working dir is kinda useful
-export PS1="\[\033[01;34m\]\w\[\033[00m\]\$"
+export PS1="\[\033[01;34m\]\w\[\033[00m\]\$ "
 sleep 1; wait.sh
